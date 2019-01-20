8b3c52cc860bb112e7c18cb913685c74635c6056
Quirino Lo Russo <quirimmo@gmail.com>
2019-01-20 06:29:19 +0100

    going on working on unit tests

--- src/app/movies/movie.model.spec.ts
+++ src/app/movies/movie.model.spec.ts
@@ -9,11 +9,12 @@ const rawMovie = {
 	genres: ['action', 'thriller'],
 	rate: '8.4',
 	length: '1h 23m',
-	img: 'img.jpg'
+	img: 'img.jpg',
+	tmdb: 1
 };
 
 describe('Movie Model', () => {
-	let movie;
+	let movie: Movie;
 	beforeEach(() => {
 		movie = new Movie(
 			1,
@@ -23,7 +24,8 @@ describe('Movie Model', () => {
 			[GenreType.Action, GenreType.Thriller],
 			'8.4',
 			'1h 23m',
-			'img.jpg'
+			'img.jpg',
+			1
 		);
 	});
 
@@ -38,6 +40,62 @@ describe('Movie Model', () => {
 	describe('buildInstanceFromRaw', () => {
 		it('should build the instance from the raw movie', () => {
 			expect(Movie.buildInstanceFromRaw(rawMovie)).toEqual(movie);
+		});
+	});
+
+	describe('buildDetails', () => {
+		const info = {
+			budget: 10,
+			homepage: 'www.homepage.movie',
+			overview: 'overview',
+			release_date: '1986-10-28',
+			revenue: 20
+		};
+		const contents = {
+			results: [
+				{
+					id: 1,
+					key: 'key 1',
+					name: 'name 1',
+					site: 'youtube',
+					type: 'trailer'
+				},
+				{
+					id: 1,
+					key: 'key 1',
+					name: 'name 1',
+					site: 'youtube',
+					type: 'trailer'
+				}
+			]
+		};
+		it('should build the details and assign them to the instance', () => {
+			expect(movie.details).toBeUndefined();
+			movie.buildDetails(info, contents);
+			expect(movie.details).toBeDefined();
+			expect(movie.details).toEqual({
+				budget: 10,
+				homepage: 'www.homepage.movie',
+				overview: 'overview',
+				releaseDate: new Date('1986-10-28'),
+				revenue: 20,
+				videos: [
+					{
+						id: 1,
+						key: 'key 1',
+						name: 'name 1',
+						site: 'youtube',
+						type: 'trailer'
+					},
+					{
+						id: 1,
+						key: 'key 1',
+						name: 'name 1',
+						site: 'youtube',
+						type: 'trailer'
+					}
+				]
+			});
 		});
 	});
 });
--- src/app/movies/movies.dao.spec.ts
+++ src/app/movies/movies.dao.spec.ts
@@ -3,16 +3,15 @@ import Movie from '../movies/movie.model';
 import GenreType from '../movies/genre.model';
 import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
-import { MoviesDAOService } from './movies.dao';
 
 const rawMovie = {
 	id: 1,
 	key: 'key',
 	name: 'name',
 	description: 'description',
-	genres: ['action, thriller'],
+	genres: ['action', 'thriller'],
 	rate: '8.4',
-	length: '1h 23 m',
+	length: '1h 23m',
 	img: 'img.jpg',
 	tmdb: 1
 };
@@ -28,9 +27,9 @@ const movie = new Movie(
 	1
 );
 const movies = [movie];
-const spyBuildInstanceFromRaw = jest
-	.spyOn(Movie, 'buildInstanceFromRaw')
-	.mockReturnValue(movie);
+const mockBuildInstanceFromRaw = jest.fn().mockReturnValue(movie);
+Movie.buildInstanceFromRaw = mockBuildInstanceFromRaw.bind(Movie);
+
 const mockGet = jest.fn().mockReturnValue(of([rawMovie]));
 const mochHttpClient = {
 	provide: HttpClient,
@@ -38,10 +37,21 @@ const mochHttpClient = {
 		get: mockGet
 	}
 };
+
+import {
+	MoviesDAOService,
+	LOCAL_DATA,
+	TMDB_API_BASE_URL,
+	TMDB_API_MOVIE_ENDPOINT,
+	TMDB_API_MOVIE_ENDPOINT_URL,
+	TMDB_API_VIDEOS_ENDPOINT,
+	TMDB_API_KEY,
+	TMDB_API_KEY_QUERY_PARAM
+} from './movies.dao';
 const providers = [mochHttpClient, MoviesDAOService];
 
 describe('MovieDAO Service', () => {
-	let service;
+	let service: MoviesDAOService;
 	beforeEach(() => {
 		jest.useFakeTimers();
 		TestBed.configureTestingModule({
@@ -49,20 +59,96 @@ describe('MovieDAO Service', () => {
 		});
 		service = TestBed.get(MoviesDAOService);
 	});
-
-	it('should call the get method of Httpclient', () => {
-		expect(mockGet).toHaveBeenCalledWith('assets/movies.mock-data.json');
+	afterEach(() => {
+		jest.clearAllMocks();
+		jest.restoreAllMocks();
 	});
 
-	it('should call the buildInstanceFromRaw method of Movie', async () => {
-		await service.movies.toPromise();
-		expect(spyBuildInstanceFromRaw).toHaveBeenCalledWith(rawMovie);
+	describe('constructor', () => {
+		it('should call the get method of Httpclient', () => {
+			expect(mockGet).toHaveBeenCalledWith('assets/movies.mock-data.json');
+		});
+
+		it('should call the buildInstanceFromRaw method of Movie', async () => {
+			await service.movies.toPromise();
+			expect(mockBuildInstanceFromRaw).toHaveBeenCalledWith(rawMovie);
+		});
 	});
 
 	describe('fetchMovies', () => {
 		it('should return the list of movies', async () => {
 			const returnedMovies = await service.movies.toPromise();
 			expect(returnedMovies).toEqual(movies);
+		});
+	});
+
+	describe('constants', () => {
+		it('should define the LOCAL_DATA', () => {
+			expect(LOCAL_DATA).toEqual('assets/movies.mock-data.json');
+		});
+
+		it('should define the TMDB_API_BASE_URL', () => {
+			expect(TMDB_API_BASE_URL).toEqual('https://api.themoviedb.org/3/');
+		});
+
+		it('should define the TMDB_API_MOVIE_ENDPOINT', () => {
+			expect(TMDB_API_MOVIE_ENDPOINT).toEqual('movie');
+		});
+
+		it('should define the TMDB_API_MOVIE_ENDPOINT_URL', () => {
+			expect(TMDB_API_MOVIE_ENDPOINT_URL).toEqual(
+				'https://api.themoviedb.org/3/movie'
+			);
+		});
+
+		it('should define the TMDB_API_VIDEOS_ENDPOINT', () => {
+			expect(TMDB_API_VIDEOS_ENDPOINT).toEqual('videos');
+		});
+
+		it('should define the TMDB_API_KEY', () => {
+			expect(TMDB_API_KEY).toEqual('53dac0ef884884dfee672ca40713c063');
+		});
+
+		it('should define the TMDB_API_KEY_QUERY_PARAM', () => {
+			expect(TMDB_API_KEY_QUERY_PARAM).toEqual(
+				'api_key=53dac0ef884884dfee672ca40713c063'
+			);
+		});
+	});
+
+	describe('getDetailsRequests', () => {
+		it('should return the two requests', () => {
+			expect(service.getDetailsRequests(1)).toHaveLength(2);
+		});
+
+		it('should build the two http get requests', () => {
+			service.getDetailsRequests(1);
+			expect(mockGet).toHaveBeenNthCalledWith(
+				2,
+				`https://api.themoviedb.org/3/movie/1?api_key=53dac0ef884884dfee672ca40713c063`
+			);
+			expect(mockGet).toHaveBeenNthCalledWith(
+				3,
+				`https://api.themoviedb.org/3/movie/1/videos?api_key=53dac0ef884884dfee672ca40713c063`
+			);
+			expect(service.getDetailsRequests(1)).toHaveLength(2);
+		});
+	});
+
+	describe('fetchMovieDetails', () => {
+		it('should call the getDetailsRequest method', () => {
+			const spy = jest.spyOn(service, 'getDetailsRequests');
+			service.fetchMovieDetails(movie);
+			expect(spy).toHaveBeenCalledWith(1);
+		});
+
+		it('should call the buildDetails method of movie', async () => {
+			jest.spyOn(service, 'getDetailsRequests').mockReturnValue(of([{}, {}]));
+			const spy = jest
+				.spyOn(movie, 'buildDetails')
+				.mockImplementation(() => movie);
+			await service.fetchMovieDetails(movie).toPromise();
+			expect(spy).toHaveBeenCalledWith([{}, {}], undefined);
 		});
 	});
 });
